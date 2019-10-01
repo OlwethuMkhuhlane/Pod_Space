458722662569e59e55a0c6b7b7f3c9137f5ebb28
Olwethu Mkhuhlane <olwethumkhuhlane@gmail.com>
2019-10-01 15:36:17 +0200

    Updates

---
+++ precache-manifest.46a6b2a80d6779dc3a1b4088d73b5cbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116762ffb80a0f82802af80f4f8edb91",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b8f8524f18da2f0f824e",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "4a562b23d46151f26b2f",
+    "url": "/portfolio/static/css/main.9e30cde8.chunk.css"
+  },
+  {
+    "revision": "b8f8524f18da2f0f824e",
+    "url": "/portfolio/static/js/2.4b7edf12.chunk.js"
+  },
+  {
+    "revision": "4a562b23d46151f26b2f",
+    "url": "/portfolio/static/js/main.781686f1.chunk.js"
+  },
+  {
+    "revision": "900f069047e28d3d6b05",
+    "url": "/portfolio/static/js/runtime~main.f854cbbe.js"
+  }
+]);
