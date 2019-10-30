0ed41d10d1d7c74f0c5c8594c0ec22b2bb3be3eb
OlwethuMkhuhlane <53062826+OlwethuMkhuhlane@users.noreply.github.com>
2019-10-30 05:24:30 +0200

    Add files via upload
    updated portfolio

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import landingpage from "./components/landingpage.js"
 import registerServiceWorker from "./registerServiceWorker";
 import "react-mdl/extra/material.css";
 import "react-mdl/extra/material.js";
@@ -11,6 +12,7 @@ import "react-mdl/extra/material.js";
 
 ReactDOM.render( 
 	<BrowserRouter >
+		<landingpage/>
 	    <App/>
 	</BrowserRouter>,
     document.getElementById("root")
